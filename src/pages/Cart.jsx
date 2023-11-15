6b78dd19aa7859fc21c5ff1b3449d0f87f84db1a
lysophavin18 <ly.sophavin1122004@gmail.com>
2023-11-15 10:04:59 +0700

    new changes

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "../styles/cart.css";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
 
 const Cart = () => {
-  return <div>cart</div>;
+  return (
+    <Helmet title="Cart">
+      <CommonSection title="Shopping" />
+    </Helmet>
+  );
 };
 
 export default Cart;
