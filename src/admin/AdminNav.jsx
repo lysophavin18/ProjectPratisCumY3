9198db5d3cdae5a6e27d03a632c0cfefacf09b57
sophavinly18 <https://github.com/lysophavin18/ProjectPratisCumY3>
2023-11-25 12:33:39 +0700

    update

--- src/admin/AdminNav.jsx
+++ src/admin/AdminNav.jsx
@@ -1,7 +1,77 @@
 import React from "react";
+import { Container, Row } from "reactstrap";
+import useAuth from "../custom-hooks/useAuth";
+import "../styles/admin-nav.css";
+
+import { NavLink } from "react-router-dom";
+
+const admin__nav = [
+  {
+    display: "Dashboard",
+    path: "/dashboard",
+  },
+  {
+    display: "All-Products",
+    path: "/dashboard/all-products",
+  },
+  {
+    display: "Orders",
+    path: "/dashboard/orders",
+  },
+  {
+    display: "Users",
+    path: "/dashboard/users",
+  },
+];
 
 const AdminNav = () => {
-  return <div>AdminNav</div>;
+  const { currentUser } = useAuth();
+
+  return (
+    <>
+      <header className="admin_header">
+        <div className="admin__nav-top">
+          <Container>
+            <div className="admin__nav-wrapper-top">
+              <div className="logo">
+                <h2>HOME.ARS</h2>
+              </div>
+              <div className="search-box">
+                <input type="text" placeholder="Search...." />
+                <span>
+                  <i class="ri-search-line"></i>
+                </span>
+              </div>
+              <div className="admin__nav-top-right">
+                <span>
+                  <i class="ri-notification-line"></i>
+                </span>
+                <span>
+                  <i class="ri-settings-2-line"></i>
+                </span>
+                <img src={currentUser.photoURL} alt="" />
+              </div>
+            </div>
+          </Container>
+        </div>
+      </header>
+      <section className="admin__menu p-0">
+        <Container>
+          <Row>
+            <div className="admin__navigation">
+              <ul className="admin__menu-list">
+                {admin__nav.map((item,index) => (
+                  <li className="admin__menu-item" key={index}>
+                    <NavLink to={item.path}>{item.display}</NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 };
 
 export default AdminNav;
--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -1,19 +1,24 @@
-/* eslint-disable no-unreachable */
 import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Routers from "../../routers/Routers";
+import AdminNav from "../../admin/AdminNav";
+import { useLocation } from "react-router-dom";
 
-const layout = () => {
-  return(
-  <>
-    <Header />
-    <div>
-      <Routers />
-    </div>
-    <Footer />
-  </>
+const Layout = () => {
+  const location = useLocation();
+
+  return (
+    <>
+      {location.pathname.startsWith("/dashboard") ? <AdminNav /> : <Header />}
+      <>
+        <div>
+          <Routers />
+        </div>
+        <Footer />
+      </>
+    </>
   );
 };
 
-export default layout;
+export default Layout;
